20724dee050b3d6f88a718daebcf2e71ac0a5c02
sadan <117494111+sadan4@users.noreply.github.com>
2025-01-14 19:06:28 -0500

    update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
@@ -24,7 +24,7 @@
     in
     {
       inherit packages;
-      package = import ./.;
+      flakePackage = import ./.;
     };
 }
 
