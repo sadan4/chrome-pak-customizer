226c82b8e9f90cb8fd3e7551bf4453861b8a4f6e
sadan <117494111+sadan4@users.noreply.github.com>
2025-01-14 19:08:16 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -3,28 +3,31 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      platforms = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-      packages = builtins.listToAttrs
-        (
-          builtins.map
-            (e:
-              let
-                pkgs = import nixpkgs { system = e; };
-              in
-              {
-                name = e;
-                value = {
-                  default = pkgs.callPackage ./. { };
-                };
-              })
-            platforms
-        );
+      platforms = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+      ];
+      packages = builtins.listToAttrs (
+        builtins.map (
+          e:
+          let
+            pkgs = import nixpkgs { system = e; };
+          in
+          {
+            name = e;
+            value = {
+              default = pkgs.callPackage ./. { };
+            };
+          }
+        ) platforms
+      );
     in
     {
       inherit packages;
-      flakePackage = import ./.;
+      flakePackage = (p: p.callPackage ./. { });
     };
 }
-
